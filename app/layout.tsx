d48831518f8e13415c3e6a4a0b881d73de92f2b1
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-11-29 10:42:38 -0500

    fix: next.js metadata warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import type { Viewport } from 'next';
 import '@/styles/app.scss';
 
 export const metadata: Metadata = {
@@ -7,13 +8,18 @@ export const metadata: Metadata = {
         default: 'Bookmarks',
     },
     description: 'Bookmarks',
-    viewport: 'width=device-width, initial-scale=1,  viewport-fit=cover',
     icons: {
         shortcut: '/favicon.ico',
         icon: '/favicons/icon.png',
         apple: '/favicons/apple-icon.png',
     },
     manifest: '/manifest.json',
+};
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    viewportFit: 'cover',
 };
 
 export default function RootLayout({
