f6012784c95683112f7630940834405389c19b1e
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-09-13 11:11:40 -0400

    feat: add next frontend

---
+++ backend/scripts/diff.ts
@@ -0,0 +1,12 @@
+import { readdir } from 'fs/promises';
+
+const files = await readdir('../screenshots');
+const ids = files.map((file) => parseInt(file.split('-')[0])).sort((a, b) => a - b);
+const missing: number[] = [];
+
+for (let i = 0; i <= 5676; i++) {
+    if (ids.includes(i)) continue;
+    missing.push(i);
+}
+
+console.log(missing);
---
+++ backend/scripts/screenshot.ts
@@ -0,0 +1,54 @@
+import { access } from 'fs/promises';
+import { chromium } from 'playwright';
+import path from 'path';
+import chalk from 'chalk';
+import urls from '@/data/formatted.urls';
+
+console.log(`\nTaking ${urls.length} screenshots...\n`);
+
+const browser = await chromium.launch();
+const context = await browser.newContext();
+
+for (let i = 5675; i < urls.length; i++) {
+    const { url } = urls[i];
+    const filename = path.join('./screenshots', `${i}-fullpage.png`);
+    const exists = await checkFileExistsAsync(filename);
+
+    if (exists) {
+        console.log(`${i},${chalk.cyan('Exists')},${url}`);
+
+        continue;
+    }
+
+    const page = await context.newPage();
+
+    try {
+        console.log(`${i},${chalk.gray('trying')},${url}`);
+
+        // await page.goto(url, { timeout: 5000 });
+        await page.goto(url);
+        await page.screenshot({ path: filename, fullPage: true });
+
+        console.log(`${i},${chalk.green('Succeeded')},${url}`);
+    } catch (err) {
+        console.log(`${i},${chalk.red('Failed')},${url}`);
+    } finally {
+        await page.close().catch(() => {});
+    }
+}
+
+async function checkFileExistsAsync(filePath: string) {
+    try {
+        await access(filePath);
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+browser?.close().catch(() => {});
+
+console.log(`\nDone.\n`);
+
+process.exit(0);
